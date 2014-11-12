57abdd6c8f71f5aa494295c6d21a56c5f489e05a
Rafael F. de Queiroz <rafaelfdequeiroz@MacBook-Pro-de-Rafael.local>
2014-11-12 18:33:41 -0300

    Preparando strategy de leitura de dados.

---
+++ src/utils/FileReaderStrategy/DataReader.java
@@ -0,0 +1,5 @@
+public interface DataReader {
+
+	public void readData();
+
+}
---
+++ src/utils/FileReaderStrategy/FileReader.java
@@ -0,0 +1,10 @@
+public class FileReader implements DataReader {
+
+	private static final String PATH_OF_FILE = "";
+
+	public void readFile() {
+		//TODO
+		//Instanciar um InputStream passando o path e realizar a leitura.
+		//Para cada linha lida, enviar para a classe GraphBuilder que irá criar os nodos e arestas.
+	}
+}
