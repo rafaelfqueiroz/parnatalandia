e3a431fb318125eeea128e903ec92da2b10f8f89
Rafael F. de Queiroz <rafaelfdequeiroz@MacBook-Pro-de-Rafael.local>
2014-11-23 23:31:49 -0300

    Corrigido bug da classe Result

--- src/domain/Result.java
+++ src/domain/Result.java
@@ -1,11 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result{
 	private Set betterSet;
 	private List<Set> sets;
 
+	public Result() {
+		this.betterSet = new Set();
+		this.sets = new ArrayList<Set>();
+	}
+	
 	public void setBetterSet(Set set){
 		this.betterSet = set;
 	}
